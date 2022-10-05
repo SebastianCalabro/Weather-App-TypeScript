df560011af0da72d49bdc9179bf27fe763eba91e
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-10-05 14:19:11 -0300

    fixing vh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
+import s from "./App.module.css"
 import { NavBar } from "./Components/NavBar/NavBar"
 import { Home } from "./Components/Home/Home"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
+    <div className={s.box}>
       <Router>
         <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
+    </div>
   );
 }
 
